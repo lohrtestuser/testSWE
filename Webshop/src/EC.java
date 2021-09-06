16eacba39349bfe988bd6ed98e22ae7a4600cc56
etodarica <etodarica@users.noreply.github.com>
2021-09-06 10:49:31 +0200

    Kommentar entfernt

--- Webshop/src/EC.java
+++ Webshop/src/EC.java
@@ -4,8 +4,7 @@ public class EC implements Onlinezahlung {
 	public EC() {
 		super();
 	}
-	//Testkommentar
-	//noch ein Testkommentar :)
+
 	@Override
 	public void erzeugeZahlung() {
 		System.out.println("Zahlungsmethode EC.");
